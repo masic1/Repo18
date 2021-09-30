016b8755a4096e8628e104b4d0d2006097bf1112
Mirza Masic <masic.mirza@gmail.com>
2021-09-30 15:22:55 +0200

    Initial

---
+++ ex01/src/index.js
@@ -0,0 +1,19 @@
+let myArr = [];
+// Only change code below this line
+function* multiplication(x) {
+    for(var i = 1; i < x; i++) {
+        yield x * 2;
+        myArr.push(x)
+    }
+} 
+let num = multiplication(3);
+
+for(var i = 1; i < 4; i++) {
+    num.next();
+}
+
+
+console.log(multiplication(3).next().value);
+console.log(myArr);
+// Only change code above this line
+module.exports = multiplication;
